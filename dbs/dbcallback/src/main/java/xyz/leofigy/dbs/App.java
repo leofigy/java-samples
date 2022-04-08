fa1c4dfafde0262772f84f3f916531497ec83baf
leofigy <angel.fig@yandex.com>
2022-04-08 13:13:07 -0500

    skeleton to do poc for ucp cost/callback in jdbc

---
+++ dbs/dbcallback/src/main/java/xyz/leofigy/dbs/App.java
@@ -0,0 +1,13 @@
+package xyz.leofigy.dbs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
